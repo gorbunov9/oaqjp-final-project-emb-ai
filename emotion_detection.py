ad24ff2026add131a2eb8e0f01124b307a2df321
Vladimir Gorbunov <gorbunov9@aol.com>
2025-04-06 03:15:00 -0400

    Replace detector

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,12 +1,13 @@
 import requests
 
 def emotion_detector(text_to_analyse):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
     myobj = { "raw_document": { "text": text_to_analyse } }
 
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
     response = requests.post(url, json=myobj, headers=header)
 
     return response.text
+    
